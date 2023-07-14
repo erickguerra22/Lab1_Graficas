d2bc349061ba87f4edc92d7607e9b676e2c41753
Erick Guerra <erickguerra2201@gmail.com>
2023-07-14 13:20:11 -0600

    Poligon-1

--- src/bitmap.h
+++ src/bitmap.h
@@ -7,5 +7,22 @@ void render()
 {
     setClearColor(Color(27, 27, 27));
     clear();
+
+    // Figura 1
+    setCurrentColor(Color(255, 255, 255));
+    std::vector<Vertex2> polygon1 = {
+        {165, 380},
+        {185, 360},
+        {180, 330},
+        {207, 345},
+        {233, 330},
+        {230, 360},
+        {250, 380},
+        {220, 385},
+        {205, 410},
+        {193, 383}};
+
+    drawPoligon(polygon1, Color(255, 255, 0));
+
     renderBuffer();
 }
